a4eba2b456b3da3507474a8d391351506a8dc7d2
lorenz234 <lorenz_lehmann@yahoo.de>
2024-10-31 13:07:54 +0100

    add dependencies

--- resolver.sol
+++ resolver.sol
@@ -19,10 +19,10 @@ contract OwnerRecipientResolver is SchemaResolver {
         }
 
         // First we check if a OwnableCheck should be performed
-        address OwnableCheck = extractOwnableCheckFromData(attestation.data);
+        bool ownableCheck = extractOwnableCheckFromData(attestation.data);
 
         // If OwnableCheck false, allow attestation
-        if (!OwnableCheck) {
+        if (!ownableCheck) {
             return true;
         }
 
@@ -33,12 +33,14 @@ contract OwnerRecipientResolver is SchemaResolver {
             return false;
         }
 
-        // Second we make sure the input chain_id matches with chainId
-        uint256 chainId;
+        // Second we make sure the input chain_id matches chainId
+        uint256 currentChainId;
         assembly {
-            chainId := chainid()
+            currentChainId := chainid()
         }
-        if (chainId != chain_id) {
+        
+        uint256 expectedChainId = extractChainIdFromData(attestation.data);
+        if (currentChainId != expectedChainId) {
             return false;
         }
 
@@ -54,19 +56,30 @@ contract OwnerRecipientResolver is SchemaResolver {
         }
     }
 
-    function extractOwnableCheckFromData(bytes memory data) internal pure returns (bool) {
-        require(data.length >= 96, "Data too short");
+    function extractChainIdFromData(bytes memory data) internal pure returns (uint256) {
+        require(data.length >= 320, "Data too short"); // 10 * 32 bytes = 320 bytes
         
-        // The value is at the same [5] position, but we're only interested in the last bit
+        // Extract the chain ID from position 10
         uint256 extractedValue;
         assembly {
-            extractedValue := mload(add(data, 96)) // Load the first 96 bytes
+            extractedValue := mload(add(data, 320)) // Load the chain ID value from position 10 (10 * 32 bytes)
+        }
+        
+        return extractedValue;
+    }
+
+    function extractOwnableCheckFromData(bytes memory data) internal pure returns (bool) {
+        require(data.length >= 352, "Data too short");
+        
+        // Extract the OwnerCheck from position 11, but we're only interested in the last bit
+        uint256 extractedValue;
+        assembly {
+            extractedValue := mload(add(data, 352)) // Load the first 96 bytes
             extractedValue := and(extractedValue, 0x1) // Mask only the last bit
         }
         
         return extractedValue == 1;
     }
-
 
     function onRevoke(Attestation calldata /*attestation*/, uint256 /*value*/) internal pure override returns (bool) {
         return true;
